cf049f08c08bd1aa99eb121da07c095b829d1148
Henrique Atila <henrique.atila@gmail.com.br>
2021-02-10 12:23:14 -0300

    Cupom -> Order Schema

--- database/migrations/1612970195922_order_schema.js
+++ database/migrations/1612970195922_order_schema.js
@@ -7,8 +7,13 @@ class OrderSchema extends Schema {
   up () {
     this.create('orders', (table) => {
       table.increments()
+      table.decimal('total', 12,2).defaultTo(0.0)
+      table.integer('user_id').unsigned()
+      table.enu('status', ['pending', 'canceled', 'shipped', 'paid', 'finished'])
+      table.foreign('user_id').references('id').inTable('users').onDelete('cascade')
       table.timestamps()
     })
+
   }
 
   down () {
